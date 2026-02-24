1750d1693fbcddd5d46430034da498f9585eb7d3
Nick Abson <4947488+nickabs@users.noreply.github.com>
2026-02-24 10:31:05 +0000

    wip

--- app/internal/pint/transport_document_state.go
+++ app/internal/pint/transport_document_state.go
@@ -1,6 +1,7 @@
 package pint
 
-// State machine for managing the state of an eBL as it is processed by the platform
+// State machine for managing the state of an eBL as it is processes new envelopes for the eBL
+// see also ebl/action_codes.go for the state machine for the transfer chain (checks internal logic of the transfer chain)
 
 import (
 	"fmt"
@@ -9,13 +10,6 @@ import (
 	"github.com/google/uuid"
 	"github.com/information-sharing-networks/pint-demo/app/internal/ebl"
 )
-
-// this state machine determines if an action code can be accepted by a platform for a given eBL
-// the rules are:
-// if platform A accepts a transfer from B then further requests from B for the same eBL are denied
-// if platform A accepts a SURR action no further request are allowed on the eBL
-// if platform A accepts an endorsement from platform B then the next action must be a transfer from B to A
-// if platform A accepts any action from platform B, all requests from other platforms for the same BL are denied
 
 // CurrentTransportDocumentState is the current state of the eBL as stored in the database
 type CurrentTransportDocumentState struct {
@@ -38,53 +32,92 @@ type requestedAction struct {
 	PlatformCode string
 }
 
-// CanAccept determines if the next state is valid given the current state
+// CanAccept determines if the platform can accept the requested action for an eBL
+//
+// return values:
 // A reason is returned in either case for logging/error reporting
 // if an error is returned it indicates a bug in the code as an unexpected combination of current and next state was received
 func (current *CurrentTransportDocumentState) CanAccept(next requestedAction, thisPlatformCode string) (accept bool, reason string, error error) {
 
+	// todo - call ebl state transition to make sure we are not creating an invalid chain
+	// some checks are repeated below as 'belt and braces' and for clarity
+	// this function just needs to do the platform specific checks
+
+	// handle in progress actions
+	// TODO: Where a transfer is pending because the sending platform sent one or more impossible additional document checksums,
+	// the sender can never complete the transfer and the BL is stuck permanently at "started but not accepted".
+	// Do we need a VOID state to cancel these actions?
 	if !current.Accepted {
-		return false, fmt.Sprintf("previous action (%s) not yet accepted for this eBL", current.ActionCode), nil
+		return false, fmt.Sprintf("previous action (%s) is in progress and not yet accepted for this eBL", current.ActionCode), nil
 	}
 
+	// handle surrender actions (these block further actions)
 	switch current.ActionCode {
-	case ebl.ActionCodeSurrenderForDelivery, ebl.ActionCodeSurrenderForAmendment:
 
+	// a previously surrendered eBL block all actions unitl the carrier responds
+	case ebl.ActionCodeSurrenderForDelivery, ebl.ActionCodeSurrenderForAmendment:
 		return false, "surrender already pending for this eBL", nil
 
+	// accepted surrender blocks all further actions
 	case ebl.ActionCodeSACC:
 		return false, "eBL already surrendered, no further action permitted", nil
 
-	case ebl.ActionCodeSign, ebl.ActionCodeIssue:
+	// transfers block further actions from the same platform
+	case ebl.ActionCodeTransfer:
+		if current.ReceivedByPlatformCode == next.PlatformCode {
+			return false, fmt.Sprintf("transfer was already acccepted from %s", current.ReceivedByPlatformCode), nil
+		}
+	}
 
-		// sign and issue do not block subsequent actions
+	// check if the next action is compatible with the current state of the eBL
+	switch next.ActionCode {
+	case ebl.ActionCodeIssue:
+		// internal error - issuance is a one time action so should not appear in the state machine
+		return false, "internal error", fmt.Errorf("issuance is a one time action so should not appear in the state machine")
+
+	case ebl.ActionCodeSign:
+		// platforms can sign at anytime and can pass to another platform for non-repudiation purposes(TODO - is this right?)
 		return true, fmt.Sprintf("action %s accepted for this eBL (current state: %s)", next.ActionCode, current.ActionCode), nil
 
 	case ebl.ActionCodeTransfer:
-
-		if !current.Accepted {
-			return false, "transfer already pending for this eBL", nil
-		}
-
-		if next.PlatformCode == thisPlatformCode {
-			return false, "internal error", fmt.Errorf("%s can't transfer to itself", thisPlatformCode)
-		}
-
+		// internal error - the current platform must be the recipient of the last accepted transfer
 		if current.ReceivedByPlatformCode != thisPlatformCode {
 			return false, "internal error", fmt.Errorf("internal error: the current platform %s is not the recipient of the last accepted transfer (%s)", thisPlatformCode, current.ReceivedByPlatformCode)
 		}
+
+		switch {
+		// transfer must be addressed to the current platform
+		case next.PlatformCode != thisPlatformCode:
+			return false, "internal error", fmt.Errorf("%s cannot proccess a transfer addressed to: %s", thisPlatformCode, next.PlatformCode)
+
+		// the current platform must be the recipient of the last accepted transfer
+		case current.ReceivedByPlatformCode != thisPlatformCode:
+			return false, "internal error", fmt.Errorf("internal error: the current platform %s is not the recipient of the last accepted transfer (%s)", thisPlatformCode, current.ReceivedByPlatformCode)
+
+		// transfers must be previously endorsed to the recipient platform
+		case current.ActionCode != ebl.ActionCodeEndorse && current.ActionCode != ebl.ActionCodeEndorseToOrder && current.ActionCode != ebl.ActionCodeBlankEndorse:
+			return false, "internal error", fmt.Errorf("transfer can only be accepted after an endorsement, (have %s)", current.ActionCode)
+		}
+
 		return true, fmt.Sprintf("transfer accepted from %s to %s", current.ReceivedByPlatformCode, next.PlatformCode), nil
 
 	case ebl.ActionCodeSREJ:
-
+		// SREJ can only be accepted after a surrender request
 		if current.ActionCode != ebl.ActionCodeSurrenderForAmendment &&
 			current.ActionCode != ebl.ActionCodeSurrenderForDelivery {
 			return false, fmt.Sprintf("SREJ can only be accepted after SURRENDER_FOR_AMENDMENT or SURRENDER_FOR_DELIVERY, (have %s)", current.ActionCode), nil
 		}
 
-		return true, "acknowldegement of rejected surrender request for this eBL accepted", nil
+		return true, "accepted recipt of rejected surrender for this eBL", nil
+
+	case ebl.ActionCodeEndorse, ebl.ActionCodeEndorseToOrder, ebl.ActionCodeBlankEndorse:
+		return true, fmt.Sprintf("endorsement accepted for this eBL (current state: %s)", current.ActionCode), nil
 	}
-	return false, "internal error", fmt.Errorf("unhandled action code, %s", current.ActionCode)
+	return false, "internal error", fmt.Errorf("bug: unhandled eBL transition, %s", current.ActionCode)
 }
 
-// todo state CanAccept CanDeliver
+// TODO: CanDeliver()
+// TODO: HasPossession()
+// TODO: IsSurenderRequested()
+// TODO: IsSurrenderAccepted()
+// TODO: IsSurrenderRejected()
